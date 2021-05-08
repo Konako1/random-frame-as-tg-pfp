7dd1f5a6d96541ea3a3c158d4007b45beeb7e2d9
Cocknako <79690555+Cocknako@users.noreply.github.com>
2021-05-08 19:16:48 +0500

    Update set_avatar.py
    - Add about updater
    - minor updates

--- set_avatar.py
+++ set_avatar.py
@@ -1,4 +1,5 @@
 from pyrogram import Client
+from pyrogram.raw import functions
 from config import id, hash
 from bad_apple import get_bad_apple_frame
 from datetime import datetime, timedelta
@@ -12,10 +13,21 @@ api_hash = hash
 async def main():
     while True:
         async with Client('my_account', api_id, api_hash) as app:
-            get_bad_apple_frame()
+            frame = get_bad_apple_frame()
 
             app: Client
+
             await app.set_profile_photo(photo='image\\pic.jpg')
+
+            await app.send(
+                functions.account.UpdateProfile(
+                    about=f'Current frame: {frame}'
+                )
+            )
+
+            photos = await app.get_profile_photos("me", limit=2)
+
+            await app.delete_profile_photos(photos[1].file_id)
 
             time_now = datetime.now()
             time_to_round = datetime.now() + timedelta(hours=1)
@@ -29,7 +41,7 @@ async def main():
             sleep_time = time_to_sleep - time_now
             sleep_time = sleep_time.total_seconds()
 
-            print(f"{sleep_time} seconds to reset\n")
+            print(f"\n{sleep_time} seconds to reset\n")
             await asyncio.sleep(sleep_time)
 
 if __name__ == '__main__':
