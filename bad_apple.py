7cdd288fd5aeb1eefa899ae4d18b7ed5726d76d9
Cocknako <79690555+Cocknako@users.noreply.github.com>
2021-05-08 19:14:49 +0500

    Update get bad apple frame

--- bad_apple.py
+++ bad_apple.py
@@ -1,15 +1,13 @@
 import cv2
 import random
 
+cap = cv2.VideoCapture('bad apple.mp4')
+video_length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
 
 def get_bad_apple_frame():
-    cap = cv2.VideoCapture('bad apple.mp4')
-
-    video_length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     random_frame = random.randint(0, video_length)
-
     print(f'Frame: {random_frame}/{video_length}')
-
     cap.set(cv2.CAP_PROP_POS_FRAMES, random_frame)
 
     while cap.isOpened():
@@ -17,7 +15,7 @@ def get_bad_apple_frame():
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         cv2.imwrite('image\\pic.jpg', gray)
         print('done!\n')
-        break
+        return random_frame
 
 
 def get_every_bad_apple_frame():
